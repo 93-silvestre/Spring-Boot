460f02ac9c39a532925b01f3ec2df72772b39754
93-silvestre <nathyribsilvestre@gmail.com>
2021-08-09 16:32:57 -0300

    Atualização LojaGames Security

--- lojagames/src/main/java/br/com/lojagames/model/Produto.java
+++ lojagames/src/main/java/br/com/lojagames/model/Produto.java
@@ -1,11 +1,15 @@
 package br.com.lojagames.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "Produtos")
 public class Produto {
 	
 	@Id
---
+++ lojagames/src/main/java/br/com/lojagames/service/UsuarioService.java
@@ -0,0 +1,25 @@
+package br.com.lojagames.service;
+
+import java.util.Optional;
+
+import br.com.lojagames.model.Usuario;
+import br.com.lojagames.model.UsuarioLogin;
+
+public class UsuarioService {
+
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Optional<Usuario> logarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
